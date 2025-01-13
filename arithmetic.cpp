456a0e09fb89ba144becb53fb2823b54a650ba63
Tasalima Jamadar <143584159+Tasalima-25@users.noreply.github.com>
2025-01-13 02:07:04 -0800

    Create arithmetic.cpp

---
+++ arithmetic.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int val1,val2,add,sub,mul,div;
+  cout<<"Enter Values"<<endl;
+  cin>>val1>>val2;
+  
+  add=val1+val2;
+  sub=val1-val2;
+  mul=val1*val2;
+  div=val1/val2;
+
+  cout<<"Addition:"<<add<<endl;
+  cout<<"Substraction:"<<sub<<endl;
+  cout<<"Multiplication:"<<mul<<endl;
+  cout<<"Division:"<<div<<endl;
+  
+  return 0;
+}
+
+
+
